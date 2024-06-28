25c1450f3c01cb25a80a09a29d8725d59002bb61
Fabian <acostahf4@gmail.com>
2024-06-28 16:42:47 -0500

    landing page desktop v1

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -5,19 +5,23 @@ import { cookies } from "next/headers";
 // This route is used to store the leads that are generated from the landing page.
 // The API call is initiated by <ButtonLead /> component
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+	const body = await req.json();
 
-  if (!body.email) {
-    return NextResponse.json({ error: "Email is required" }, { status: 400 });
-  }
+	if (!body.email) {
+		return NextResponse.json(
+			{ error: "Email is required" },
+			{ status: 400 }
+		);
+	}
 
-  try {
-    const supabase = createRouteHandlerClient({ cookies });
-    await supabase.from("leads").insert({ email: body.email });
+	try {
+		const supabase = createRouteHandlerClient({ cookies });
+		await supabase.from("leads").insert({ email: body.email });
 
-    return NextResponse.json({});
-  } catch (e) {
-    console.error(e);
-    return NextResponse.json({ error: e.message }, { status: 500 });
-  }
+		return NextResponse.json({});
+	} catch (e) {
+		console.error(e);
+		console.log("Error: ", e.message);
+		return NextResponse.json({ error: e.message }, { status: 500 });
+	}
 }
