aca37061aede2df15889412f8b536e2e22d51800
Fabian <acostahf4@gmail.com>
2024-07-18 14:46:34 -0500

    update remotePatterns

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
 			"logos-world.net",
 			"unsplash.com",
 		],
+		remotePatterns: [
+			// NextJS <Image> component needs to whitelist domains for src={}
+			"https://lh3.googleusercontent.com/*",
+			"https://pbs.twimg.com/*",
+			"https://images.unsplash.com/*",
+			"https://logos-world.net/*",
+			"https://unsplash.com/*",
+			"https://anana-landing.vercel.app/*",
+		],
 	},
 };
 
