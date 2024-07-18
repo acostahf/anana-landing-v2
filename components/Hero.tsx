a480a6d31e5197c8b39f1745f252809d108e231a
Fabian <acostahf4@gmail.com>
2024-07-18 14:58:47 -0500

    update location and fetch

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,18 +7,18 @@ import useMeasure from "react-use-measure";
 
 const Hero = () => {
 	const [items, setItems] = useState([
-		"/app/assets/Frame_7.png",
-		"/app/assets/Frame_12.png",
-		"/app/assets/Frame_13.png",
+		"/assets/Frame_7.png",
+		"/assets/Frame_12.png",
+		"/assets/Frame_13.png",
 	]);
 	const [secondItems, setSecondItems] = useState([
-		"/app/assets/Image_26.png",
-		"/app/assets/Image_37.png",
+		"/assets/image_26.png",
+		"/assets/image_37.png",
 		"https://images.unsplash.com/photo-1447279506476-3faec8071eee?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 	]);
 	const [thirdItems, setThirdItems] = useState([
-		"/app/assets/Image_29.png",
-		"/app/assets/Image_28.png",
+		"/assets/image_29.png",
+		"/assets/image_28.png",
 		"https://images.unsplash.com/photo-1606584178861-09b1d9149487?q=80&w=2427&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 	]);
 
@@ -87,6 +87,7 @@ const Hero = () => {
 								priority={true}
 								fill={true}
 								style={{ objectFit: "cover" }}
+								fetchPriority="high"
 							/>
 						</motion.div>
 					))}
--- next.config.js
+++ next.config.js
@@ -2,22 +2,29 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: [
-			// NextJS <Image> component needs to whitelist domains for src={}
-			"lh3.googleusercontent.com",
-			"pbs.twimg.com",
-			"images.unsplash.com",
-			"logos-world.net",
-			"unsplash.com",
-		],
+		// domains: [
+		// 	// NextJS <Image> component needs to whitelist domains for src={}
+		// 	"lh3.googleusercontent.com",
+		// 	"pbs.twimg.com",
+		// 	"images.unsplash.com",
+		// 	"logos-world.net",
+		// 	"unsplash.com",
+		// 	"anana-landing.vercel.app",
+		// ],
 		remotePatterns: [
 			// NextJS <Image> component needs to whitelist domains for src={}
-			"https://lh3.googleusercontent.com/*",
-			"https://pbs.twimg.com/*",
-			"https://images.unsplash.com/*",
-			"https://logos-world.net/*",
-			"https://unsplash.com/*",
-			"https://anana-landing.vercel.app/*",
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com/*",
+			},
+			{
+				protocol: "https",
+				hostname: "anana-landing.vercel.app/*",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost:3000/*",
+			},
 		],
 	},
 };
