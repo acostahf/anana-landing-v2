69568d6815ed8a0492a6a6705871cd03c909f271
Fabian <acostahf4@gmail.com>
2025-04-06 19:20:50 -0500

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { Suspense } from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
-
+import { redirect } from "next/navigation";
 export default function Home() {
+    //redirect to www.cookwithanana.com
+    redirect("https://www.cookwithanana.com");
     return (
         <div className="flex justify-center">
             <div className="w-full overflow-hidden relative ">
